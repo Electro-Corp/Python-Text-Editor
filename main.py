ad882a1eb37ff88bbeaeb66f6f553e2e9904ce4d
Electro-Corp <chinmaytanmay@outlook.com>
2022-04-10 10:24:31 -0600

    Added basic syntax highlighting
    Added basic syntax highlighting, will add more in the future

--- main.py
+++ main.py
@@ -3,16 +3,26 @@ from tkinter import messagebox
 from tkinter import Tk, Frame, Menu
 import tkinter.font as font
 import os
+syntax = {
+  "print" : "purple",
+  "if": "orange",
+  "True": "blue",
+  "else": "orange",
+  "False": "blue"
+}
 class TextEditor:
   def __init__(self,root):
     #info needed 
     self.currentfont = 'Arial'
+    
     # Window Details
     self.hello = tk.Label(text="Text Editor")
     self.hello.pack()
     #Main Text Box
-    self.box = tk.Text(root,height=15,width=30)  
+    self.box = tk.Text(root,height=10,width=30)  
     self.box.configure(font = 'Arial')
+    for key in syntax:
+      self.box.tag_configure(key, background="white", foreground=syntax[key])
     self.box.pack()
     #Save as textbox
     self.saveas = tk.Text(root,height=1,width=10)
@@ -22,9 +32,10 @@ class TextEditor:
     self.button.pack()
     self.open = tk.Button(command=self.read,text="Open")
     self.open.pack()
-    #tool bar for font changes and the such
+    #create toolbar thing
     menubar = Menu(root)
     root.config(menu=menubar)
+    #file menu (lol)
     fileMenu = Menu(menubar)
     fileMenu.add_command(label="Times New Roman", command=lambda: self.changeFont('Times New Roman') )
     fileMenu.add_command(label="Courier", command=lambda: self.changeFont('Courier'))
@@ -33,6 +44,10 @@ class TextEditor:
     #Platino Linotype
     fileMenu.add_command(label="Platino Linotype", command=lambda: self.changeFont('Platino Linotype'))
     menubar.add_cascade(label="Font", menu=fileMenu)
+    #code highlighting
+    codeMenu = Menu(menubar)
+    fileMenu.add_command(label="[Python] Refresh Syntax Highlighting ",command=lambda: self.syntaxHighlight('Default'))
+    codeMenu.add_cascade(label="Syntax", menu=menubar)
   #Change font
   def save(self):  
     b = self.saveas.get("1.0",'end-1c')
@@ -82,7 +97,7 @@ class TextEditor:
           self.changeFont(str(font))
           print(font)
           for i in range(len(main)):
-            self.box.insert("1.0",main[i])
+            self.box.insert("1.0",main[len(main)-(i+1)])
       except FileNotFoundError:
         messagebox.showerror(title="File Not found!", message="File not found. :(")
     else:
@@ -98,13 +113,44 @@ class TextEditor:
           for line in thelines:
             main.append(line)
           for i in range(len(main)):
-            self.box.insert("1.0",main[i])
+            print(len(main)-(i+1))
+            self.box.insert("1.0",main[len(main)-(i+1)])
       except FileNotFoundError:
         messagebox.showerror(title="File Not found!", message="File not found. :(")
   def changeFont(self,thecurrentfont):
     thefont = font.Font(family=thecurrentfont)
     self.box.configure(font = thefont)
     self.currentfont = thecurrentfont
+  def syntaxHighlight(self,word):
+    lines = self.box.get("1.0",'end-1c')
+    newlines = lines.split()  
+    #word = self.box.selection_get()
+    pos_start = "1.0"
+    print(newlines)
+    for line in newlines:
+      word = line
+      offset = '+%dc' % len(line)
+      print(offset)
+      prevpos_start = pos_start
+      pos_start = self.box.search(line, prevpos_start, 'end')
+      pos_end = pos_start + offset
+      print(line)
+      print(pos_start)
+      print(pos_end)
+      # index = self.box.search(line, "insert", backwards=True, regexp=True)
+      # if index == "":
+      #   index ="1.0"
+      # else:
+      #   index = self.box.index("%s+1c" % index)
+      #word = self.text.get(index, "insert")
+      if line in syntax:
+        print(line+"is in syntax")
+        #self.box.tag_add(word, "sel.first", "sel.last")
+        #self.box.tag_add(line, index, "%s+%dc" % (index, len(line)))
+        self.box.tag_add(word, pos_start, pos_end)
+    #thecolor = color.Color(rncolor)
+    #self.box.configure = (color = thecolor)
+    
 root = tk.Tk()
 root.title("Text Editor")
 root.geometry("350x400")
